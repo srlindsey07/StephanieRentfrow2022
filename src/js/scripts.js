42dfd8b4b5a66cf66bcb3c2f4d229f17abec54c5
Stephanie Rentfrow <stephanie.rentfrow@modivcare.com>
2022-03-02 10:43:00 -0500

    Update responsive styles

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,4 +3,38 @@ $(document).ready(function(){
     var now = new Date();
     var currentYear = now.getFullYear();
     $('#currentYear').text(currentYear);
+
+    function closeMenu() {
+        $('#menu').removeClass('open');
+        $('#menu').addClass('closed');
+    }
+    function openMenu() {
+        $('#menu').addClass('open');
+        $('#menu').removeClass('closed');
+    }
+
+    // menu toggle classes
+    var menuOpen = false;
+    $('#menu_toggle').click(() => {
+        menuOpen = !menuOpen;
+
+        if (menuOpen) {
+            openMenu();
+        }
+        else {
+            closeMenu();
+        }
+    });
+
+    // remove menu toggle classes when not in mobile sizes
+    $(window).on('resize', () => {
+        if (window.innerWidth >= 768) {
+            // make desktop menu
+            $('#menu').removeClass('closed');
+            $('#menu').removeClass('open');
+        }
+        else {
+            closeMenu();
+        }
+    });
 });
