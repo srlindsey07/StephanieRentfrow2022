b6bee7e1517d771224083b1f921514b33747ed5c
Stephanie Rentfrow <stephanie.rentfrow@modivcare.com>
2022-03-03 09:15:38 -0500

    Updated styles and accessibility features

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -7,10 +7,12 @@ $(document).ready(function(){
     function closeMenu() {
         $('#menu').removeClass('open');
         $('#menu').addClass('closed');
+        $('#menu_toggle').attr('aria-expanded', false);
     }
     function openMenu() {
         $('#menu').addClass('open');
         $('#menu').removeClass('closed');
+        $('#menu_toggle').attr('aria-expanded', true);
     }
 
     // menu toggle classes
@@ -32,9 +34,37 @@ $(document).ready(function(){
             // make desktop menu
             $('#menu').removeClass('closed');
             $('#menu').removeClass('open');
+            $('#menu_toggle').attr('aria-expanded', false);
         }
         else {
             closeMenu();
         }
     });
+
+    // check if element is in viewport vertically
+    jQuery.fn.isInViewport = function() {
+        var elTop = $(this).offset().top;
+        var elBottom = elTop + $(this).outerHeight();
+
+        var viewportTop = $(window).scrollTop();
+        var viewportBottom = viewportTop + $(window).innerHeight();
+
+        return elBottom > viewportTop && elTop < viewportBottom;
+    }
+
+    function toggleNavOpacity() {
+        if ($('.header-content').isInViewport()) {
+            $('.main-nav').addClass('top');
+        }
+        else {
+            $('.main-nav').removeClass('top');
+        }
+    }
+    toggleNavOpacity();
+
+    // change opacity of header on window scroll/resize if
+    // the header content is in the viewport
+    $(window).on('resize scroll', () => {
+        toggleNavOpacity();
+    })
 });
